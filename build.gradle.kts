978b2088c8fd333b706e13f16e39fe95c92696d7
jhg913915 <tima.trusov@yandex.ru>
2025-06-26 22:51:12 +0300

    added sentry error checker support

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
 	jacoco
 	id("org.springframework.boot") version "3.4.2"
 	id("io.spring.dependency-management") version "1.1.7"
+	id("io.sentry.jvm.gradle") version "5.8.0"
 }
 
 group = "hexlet.code"
