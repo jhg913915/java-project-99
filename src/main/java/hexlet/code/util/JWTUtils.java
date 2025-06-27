e312db65a6100e5ff1e3bf7a0b6f621da0c2383e
jhg913915 <tima.trusov@yandex.ru>
2025-06-27 16:31:33 +0300

    made code refactoring(fixed checkstyle errors)

--- src/main/java/hexlet/code/util/JWTUtils.java
+++ src/main/java/hexlet/code/util/JWTUtils.java
@@ -1,7 +1,6 @@
 package hexlet.code.util;
 
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.oauth2.jwt.JwtClaimsSet;
 import org.springframework.security.oauth2.jwt.JwtEncoder;
 import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
